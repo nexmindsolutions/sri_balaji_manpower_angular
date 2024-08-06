9ac704b122350f700b514928209a2634f56586a5
Lakshmi-Manogna-12613 <manumanogna126143@gmail.com>
2024-08-06 13:28:01 +0530

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
