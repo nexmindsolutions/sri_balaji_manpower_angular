68db072edffa09598d07216c7b214dd9ee1f1ebf
Nexmind <nexmindsolutions@gmail.com>
2024-08-06 13:28:27 +0530

    contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/home-page',pathMatch:'full'},
-  {path:'home-page',component:HomePageComponent}
+  {path:'home-page',component:HomePageComponent},
+  {path : 'contact', component : ContactComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
