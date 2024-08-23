c23cbd30afcd44ae839b59be8b94830bb3f203f8
Lakshmi-Manogna-12613 <manumanogna126143@gmail.com>
2024-08-23 16:52:23 +0530

    reload adjusted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,20 +6,19 @@ import { ServicesComponent } from './components/services/services.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { OurclientsComponent } from './components/ourclients/ourclients.component';
 
-
-
 const routes: Routes = [
-  // {path:'',redirectTo:'/home-page',pathMatch:'full'},
-  {path:'',component:HomePageComponent},
-  {path:'home-page',component:HomePageComponent},
-  {path : 'contact', component : ContactComponent},
-  {path:'services-page',component:ServicesComponent},
-  {path:'aboutus', component:AboutusComponent},
-  {path:'ourclients', component:OurclientsComponent}
+  { path: '', component: HomePageComponent },
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services-page', component: ServicesComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'ourclients', component: OurclientsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled' // This line enables scroll restoration
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
